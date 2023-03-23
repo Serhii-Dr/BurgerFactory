afd32281bae7d80b283ce5de61f5c1a9c8e015bc
Serhii Dranuta <sirojka8@gmail.com>
2023-03-23 21:43:07 +0200

    Details expand in meal-menu added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const elements = document.querySelectorAll('.appear');
+const items = document.querySelectorAll('.meal-item__details');
 
 window.addEventListener('scroll', function () {
   const scrollPosition = this.window.scrollY + this.window.innerHeight;
@@ -7,5 +8,14 @@ window.addEventListener('scroll', function () {
     if (scrollPosition > elemPosition) {
       element.classList.add('scrollAnimation');
     }
+  });
+});
+
+items.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.textContent === 'Details'
+      ? (item.textContent = 'Reduce')
+      : (item.textContent = 'Details');
+    item.previousElementSibling.classList.toggle('d-none');
   });
 });
