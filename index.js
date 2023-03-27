06191e1db7242cd289363e3ad3d96c2e4e29a1b3
Serhii Dranuta <sirojka8@gmail.com>
2023-03-27 21:14:39 +0300

    Burger menu added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// DOM Nodes
+
 const elements = document.querySelectorAll('.appear');
 const items = document.querySelectorAll('.meal-item__details');
+const burgerBtn = document.querySelector('.burger-menu');
+const closeIcon = document.querySelector('.close-item');
+const dropdown = document.querySelector('.burger-dropdown');
+
+// Listeners
 
 window.addEventListener('scroll', function () {
   const scrollPosition = this.window.scrollY + this.window.innerHeight;
@@ -19,3 +26,11 @@ items.forEach((item) => {
     item.previousElementSibling.classList.toggle('d-none');
   });
 });
+
+burgerBtn.addEventListener('click', function () {
+  dropdown.classList.toggle('open')
+});
+
+closeIcon.addEventListener('click', function(){
+  dropdown.classList.remove('open');
+})
