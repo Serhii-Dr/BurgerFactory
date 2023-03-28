c04a174a40f1bf9a6c8885fb727710f4e81662a1
Serhii Dranuta <sirojka8@gmail.com>
2023-03-28 21:59:28 +0300

    Slider and media for mobiles added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const items = document.querySelectorAll('.meal-item__details');
 const burgerBtn = document.querySelector('.burger-menu');
 const closeIcon = document.querySelector('.close-item');
 const dropdown = document.querySelector('.burger-dropdown');
-
+const nextArrowBtn = document.querySelector('.arrow-right');
+const prevArrowBtn = document.querySelector('.arrow-left');
+const slider = document.querySelector('.ingredients-section__slider');
 
 // Listeners
 
@@ -30,10 +32,32 @@ items.forEach((item) => {
 
 burgerBtn.addEventListener('click', function () {
   dropdown.classList.toggle('open');
-  this.classList.toggle('lines-animation')
+  this.classList.toggle('lines-animation');
 });
 
-closeIcon.addEventListener('click', function(){
+closeIcon.addEventListener('click', function () {
   dropdown.classList.remove('open');
-  burgerBtn.classList.remove('lines-animation')
-})
+  burgerBtn.classList.remove('lines-animation');
+});
+
+let currentIndex = 0;
+
+nextArrowBtn.addEventListener('click', function () {
+  const windowWidth = window.innerWidth;
+  if (currentIndex < slider.children.length - 1) {
+    currentIndex++;
+    slider.style.transform = `translateX(-${
+      currentIndex * (windowWidth > 1000 ? 100 : 250)
+    }px)`;
+  }
+});
+
+prevArrowBtn.addEventListener('click', function () {
+  const windowWidth = window.innerWidth;
+  if (currentIndex > 0) {
+    currentIndex--;
+    slider.style.transform = `translateX(-${
+      currentIndex * (windowWidth > 1000 ? 100 : 250)
+    }px)`;
+  }
+});
